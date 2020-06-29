b8d33685e3ed24cc072ec6b86489ecfc368c7a28
inyong-e <jiy8319@gmail.com>
2020-06-29 23:32:35 +0900

    Create: 영화 목록 보여주는 컴포넌트 생성
    omdb API에서 Free Key를 발급 받아서, 영화 관련 데이터를 요청해서 사용하도록 구현.
    이따가 API키는 .env 파일로 빼야할 것 같다.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,43 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 
 import "../App.css";
 
-import Header from "../components/common/Header";
+import Header from "../common/Header";
+import Movie from "./Movie";
 import Search from "./Search";
 
+interface IMovie {
+  Poster: string;
+  Title: string;
+  Type: string;
+  Year: string;
+  imdbID: string;
+}
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=59a43818";
+
 const App = () => {
+  const [movies, setMovies] = useState([] as IMovie[]);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+      });
+  });
+
   return (
     <div className="container">
       <Header text="mini-movie-app" />
       <Search />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {movies.map(movie => (
+          <Movie key={movie.Title} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 };
