0875b41faf481f31e7340b20820bb97c75d40b16
inyong-e <jiy8319@gmail.com>
2020-06-29 23:45:44 +0900

    Update: 영화 검색 기능 추가
    검색 창에 영화 검색 시, 해당 검색에 맞는 영화 리스트만 나오도록 기능 수정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,24 +19,48 @@ const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=59a43818";
 
 const App = () => {
   const [movies, setMovies] = useState([] as IMovie[]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     fetch(MOVIE_API_URL)
       .then(response => response.json())
       .then(jsonResponse => {
         setMovies(jsonResponse.Search);
+        setLoading(false);
       });
   });
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  };
 
   return (
     <div className="container">
       <Header text="mini-movie-app" />
-      <Search />
+      <Search search={search} />
       <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
-        {movies.map(movie => (
-          <Movie key={movie.Title} movie={movie} />
-        ))}
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map(movie => <Movie key={movie.Title} movie={movie} />)
+        )}
       </div>
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
 import { useState } from "react";
 
-const Search = () => {
+const Search = ({ search }) => {
   const [searchValue, setSearchValue] = useState("");
   const onChangeSearchinputChanges = (e: React.FormEvent<HTMLInputElement>) => {
     setSearchValue(e.currentTarget.value);
   };
+  const resetInputField = () => {
+    setSearchValue("");
+  };
   const onSearch = (e: React.FormEvent<HTMLInputElement>) => {
     e.preventDefault();
+    search(searchValue);
+    resetInputField();
   };
   return (
-    <form className="search-form">
+    <form className="search">
       <input
         value={searchValue}
         onChange={onChangeSearchinputChanges}
