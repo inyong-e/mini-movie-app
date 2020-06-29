0d359ba250af23d320ec11a5135d1fc30a1c2730
inyong-e <jiy8319@gmail.com>
2020-06-29 21:28:24 +0900

    set: Typescript 환경 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.min.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
