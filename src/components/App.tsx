6768fecc687454afec6d98f5d5e5f81ee36c27da
inyong-e <jiy8319@gmail.com>
2020-06-29 22:32:56 +0900

    Create: 검색 컴포넌트 세팅

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import Header from "../components/common/Header";
+import Search from "./Search";
 
 const App = () => {
   return (
     <div>
       <Header text="mini-movie-app" />
+      <Search />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { useState } from "react";
+
+const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const onChangeSearchinputChanges = (e: React.FormEvent<HTMLInputElement>) => {
+    setSearchValue(e.currentTarget.value);
+  };
+  const onSearch = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <form>
+      <input
+        value={searchValue}
+        onChange={onChangeSearchinputChanges}
+        type="text"
+      />
+      <input onClick={onSearch} type="submit" value="Search!" />
+    </form>
+  );
+};
+
+export default Search;
